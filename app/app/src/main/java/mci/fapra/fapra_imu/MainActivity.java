cb07f7c9d260ada30b09b463a53b720ca463e574
Benedict Steuerlein <bsteuerlein@web.de>
2018-11-07 12:14:48 +0100

    Initial app commit

---
+++ app/app/src/main/java/mci/fapra/fapra_imu/MainActivity.java
@@ -0,0 +1,13 @@
+package mci.fapra.fapra_imu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

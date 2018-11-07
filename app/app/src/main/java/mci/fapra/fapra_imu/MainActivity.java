dfced32047db20cb8e5faed300328f14e7dc0d7f
Benedict Steuerlein <bsteuerlein@web.de>
2018-11-07 13:30:26 +0100

    Initial classes

--- app/app/src/main/java/mci/fapra/fapra_imu/MainActivity.java
+++ app/app/src/main/java/mci/fapra/fapra_imu/MainActivity.java
@@ -2,6 +2,10 @@ package mci.fapra.fapra_imu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText participantId = (EditText) findViewById(R.id.pId);
+        Button start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
     }
 }
---
+++ app/app/src/main/java/mci/fapra/fapra_imu/Sensor.java
@@ -0,0 +1,4 @@
+package mci.fapra.fapra_imu;
+
+public class Sensor {
+}
